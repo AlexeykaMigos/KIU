51e2a2e2daeaf3314bd65e1cdc1d0c743c4039af
AlexeykaMigos <alexeyka.migos@gmail.com>
2022-11-23 18:49:40 +0300

    remake code

--- pythonProject/semestr_task.py
+++ pythonProject/semestr_task.py
@@ -1,79 +1,41 @@
 
-def summa(a):
-    counter = sum(1 for _ in a)
-    if counter != 4:
+def summa (numbers: list):
+    if len(numbers) != 4:
         return "error"
-    for i in range(len(a)):
-        if isinstance(i,int) == False:
+    for num in numbers:
+        if not isinstance(num, int):
             return "error"
-    if type(a[0]) == str or type(a[1]) == str or type(a[2]) == str or type(a[3]) == str:
+    return [float(numbers[0] + numbers[2]), float(numbers[1] + numbers[3])]
+
+
+
+
+def raznost(numbers: list):
+    if len(numbers) != 4:
         return "error"
-    a1 = a[0]
-    a2 = a[2]
-    b1 = a[1]
-    b2 = a[3]
-    z1 = a1 + a2
-    z2 = b1 + b2
-    return [z1, z2]
-
-
-
-
-def raznost(a):
-    counter = sum(1 for _ in a)
-    if counter != 4:
-        return "error"
-    for i in range(len(a)):
-        if isinstance(i,int) == False:
+    for num in numbers:
+        if not isinstance(num, int):
             return "error"
-    if type(a[0]) == str or type(a[1]) == str or type(a[2]) == str or type(a[3]) == str:
-        return "error"
-    a1 = a[0]
-    a2 = a[2]
-    b1 = a[1]
-    b2 = a[3]
-    z1 = a1 - a2
-    z2 = b1 - b2
-    return [z1, z2]
+    return [float(numbers[0] - numbers[2]), float(numbers[1] - numbers[3])]
 
 
-def multi(a):
-    counter = sum(1 for _ in a)
-    if counter != 4:
+def multi(numbers: list):
+    if len(numbers) != 4:
         return "error"
-    for i in range(len(a)):
-        if isinstance(i,int) == False:
+    for num in numbers:
+        if not isinstance(num, int):
             return "error"
-    if type(a[0]) == str or type(a[1]) == str or type(a[2]) == str or type(a[3]) == str:
-        return "error"
-    a1 = a[0]
-    a2 = a[2]
-    b1 = a[1]
-    b2 = a[3]
-    z1 = (a1 * a2) - (b1 * b2)
-    z2 = (a1 * b2) + (b1 * a2)
-    return [z1, z2]
+    return [float((numbers[0] * numbers[2]) - (numbers[1]*numbers[3])),float((numbers[0]*numbers[3]) + (numbers[1]*numbers[2]) ) ]
 
 
-def divide(a):
-    counter = sum(1 for _ in a)
-    if counter != 4:
+def divide(numbers: list):
+    if len(numbers) != 4:
         return "error"
-    for i in range(len(a)):
-        if isinstance(i,int) == False:
+    for num in numbers:
+        if not isinstance(num, int):
             return "error"
-    if type(a[0]) == str or type(a[1]) == str or type(a[2]) == str or type(a[3]) == str:
+    if (numbers[2] * numbers[2]) + (numbers[3] * numbers[3]) == 0:
         return "error"
-    a1 = a[0]
-    a2 = a[2]
-    b1 = a[1]
-    b2 = a[3]
-    if (a2*a2)+(b2*b2) == 0:
-        return "error"
-    z1 = ((a1 * a2) + (b1*b2)) / ((a2*a2)+(b2*b2))
-    z2 = ((a2 * b1) - (a1*b2)) / ((a2*a2)+(b2*b2))
-
-    return [round(z1,4), round(z2,4)]
-
+    return [round(float(((numbers[0]*numbers[2])+(numbers[1]*numbers[3])) / ((numbers[2] * numbers[2]) + (numbers[3] * numbers[3]))),4), round(float(((numbers[2]*numbers[1])-(numbers[0]*numbers[3])) / ((numbers[2] * numbers[2]) + (numbers[3] * numbers[3]))),4)]
 
 
