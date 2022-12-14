597903fd3776c736410d14ae36a71184594c44c4
AlexeykaMigos <alexeyka.migos@gmail.com>
2022-12-14 07:48:28 +0300

    remake code

--- pythonProject/semestr_task.py
+++ pythonProject/semestr_task.py
@@ -1,33 +1,32 @@
 
-def summa (numbers: list):
+def summa(numbers: list):
     if len(numbers) != 4:
         return "error"
     for num in numbers:
-        if not isinstance(num, int):
+        if not isinstance(num, int) and not isinstance(num, float):
             return "error"
-    return [float(numbers[0] + numbers[2]), float(numbers[1] + numbers[3])]
+    return [round(float(numbers[0] + numbers[2]),2), round(float(numbers[1] + numbers[3]),2)]
 def raznost(numbers: list):
     if len(numbers) != 4:
         return "error"
     for num in numbers:
-        if not isinstance(num, int):
+        if not isinstance(num, int) and not isinstance(num, float):
             return "error"
     return [float(numbers[0] - numbers[2]), float(numbers[1] - numbers[3])]
 def multi(numbers: list):
     if len(numbers) != 4:
         return "error"
     for num in numbers:
-        if not isinstance(num, int):
+        if not isinstance(num, int) and not isinstance(num, float):
             return "error"
     return [float((numbers[0] * numbers[2]) - (numbers[1]*numbers[3])),float((numbers[0]*numbers[3]) + (numbers[1]*numbers[2]) ) ]
 def divide(numbers: list):
     if len(numbers) != 4:
         return "error"
     for num in numbers:
-        if not isinstance(num, int):
+        if not isinstance(num, int) and not isinstance(num, float):
             return "error"
     if (numbers[2] * numbers[2]) + (numbers[3] * numbers[3]) == 0:
         return "error"
     return [round(float(((numbers[0]*numbers[2])+(numbers[1]*numbers[3])) / ((numbers[2] * numbers[2]) + (numbers[3] * numbers[3]))),4), round(float(((numbers[2]*numbers[1])-(numbers[0]*numbers[3])) / ((numbers[2] * numbers[2]) + (numbers[3] * numbers[3]))),4)]
-
 
